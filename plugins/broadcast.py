4f239308670625ef11abb27479711f655d17195f
LMINARSFT <93986165+LMINARSFT@users.noreply.github.com>
2021-11-11 14:52:49 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -13,7 +13,7 @@ async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='Broadcasting your messages...'
+        text='ඔයාගෙ message එක broadcast කරන ගමන් ඉන්නෙ'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
@@ -37,6 +37,6 @@ async def verupikkals(bot, message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+            await sts.edit(f"Broadcast in progress:\n\nසම්පූර්ණ පරිශීලකයන් {total_users}\nසම්පූර්ණයි: {done} / {total_users}\nසාර්ථකයි: {success}\nමාව block කරපු ගණන: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nමගේ පරිශීලකයන් ගණන {total_users}\nසම්පූර්ණයි: {done} / {total_users}\nසාර්ථකයි: {success}\nමාව block කරපු ගණන: {blocked}\nDeleteකරපු: {deleted}")
